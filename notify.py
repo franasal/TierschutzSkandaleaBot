1c8ba7c4d970efe3f418b3d467197b3ee5d9e357
El Pacho <75735030+franasal@users.noreply.github.com>
2025-05-07 18:50:42 +0200

    Update notify.py

--- notify.py
+++ notify.py
@@ -1,31 +1,37 @@
 import os
 import requests
-import telegram
 
 # Get Telegram Token and Chat ID from environment variables
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-CHAT_ID = os.getenv('CHAT_ID')
+CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 # Check if token and chat_id are set
 if not TELEGRAM_TOKEN or not CHAT_ID:
     raise ValueError("TELEGRAM_TOKEN or CHAT_ID is not set!")
 
-# Telegram bot setup
-bot = telegram.Bot(token=TELEGRAM_TOKEN)
+# Function to send a Telegram message using requests
+def send_telegram_message(message):
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    data = {"chat_id": CHAT_ID, "text": message, "parse_mode": "HTML", "disable_web_page_preview": False}
+    
+    try:
+        response = requests.post(url, data=data)
+        print(f"Telegram status: {response.status_code}")
+        print(f"Response: {response.text}")
+    except Exception as e:
+        print(f"Telegram error: {e}")
 
 # API URL for fetching posts
 API_URL = 'https://tierschutz-skandale.de/wp-json/wp/v2/posts?_embed'
 
 # Function to fetch posts from the API
 def get_posts(per_page=5, page=1):
-    url = "https://tierschutz-skandale.de/wp-json/wp/v2/posts?_embed&per_page=2"
+    url = f"https://tierschutz-skandale.de/wp-json/wp/v2/posts?_embed&per_page={per_page}&page={page}"
     headers = {"User-Agent": "Mozilla/5.0"}  # important for some servers
-
-    res = requests.get(url, headers=headers)
-    print(f"Status: {res.status_code}")
-    print(res.text)
-
+    
     try:
+        res = requests.get(url, headers=headers)
+        print(f"Status: {res.status_code}")
         res.raise_for_status()  # Will raise an error for non-200 status codes
         return res.json()
     except requests.exceptions.HTTPError as e:
@@ -34,15 +40,6 @@ def get_posts(per_page=5, page=1):
     except requests.exceptions.JSONDecodeError as e:
         print(f"JSON Error: {res.text}")
         raise
-
-# Function to send a Telegram message
-def send_telegram_message(message):
-    try:
-        response = bot.send_message(chat_id=CHAT_ID, text=message, parse_mode="HTML", disable_web_page_preview=False)
-        print(f"Message sent: {response}")
-    except Exception as e:
-        print(f"Telegram error: {e}")
-        print(CHAT_ID)
 
 # Main function to fetch posts and send messages
 def send_new_posts():
